940880e61fd166925ebe39b6591d58e0c670256b
Anna Mityaeva <aiisa@inbox.ru>
2022-11-28 06:26:21 +0300

    add Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "anna")
+{
+    Console.WriteLine("Yay! It's Anna!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
