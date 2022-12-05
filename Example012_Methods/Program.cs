1aedeb2507f7d6d0dd100cd4db0954faa8f9c3ee
Anna Mityaeva <aiisa@inbox.ru>
2022-12-05 06:14:31 +0300

    Example 12 Methods_update 2

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -56,4 +56,13 @@ string Method4(int count, string c)
     return result;
 }
 string res = Method4(10, "asdf");
-Console.WriteLine(res);
+//Console.WriteLine(res);
+
+for(int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
