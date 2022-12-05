e857758ac851c54dca50d569d1154b61bc04fb44
Anna Mityaeva <aiisa@inbox.ru>
2022-12-05 07:07:26 +0300

    update Example 12

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,34 +1,34 @@
 ﻿// Вид 1
-void Method1()
-{
-    Console.WriteLine("Author ANMI");
-}
+//void Method1()
+//{
+//    Console.WriteLine("Author ANMI");
+//}
 //Method1();
 
 // Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
+//void Method2(string msg)
+//{
+//    Console.WriteLine(msg);
+//}
 //Method2(msg: "Text");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
+//void Method21(string msg, int count)
+//{
+//    int i = 0;
+//    while (i < count)
+//    {
+//        Console.WriteLine(msg);
+//        i++;
+//    }
+//}
 //Method21(msg: "Text", count: 4);
 
 // Вид 3
-int Method3()
-{
-    return DateTime.Now.Year;
-}
-int year = Method3();
+//int Method3()
+//{
+//    return DateTime.Now.Year;
+//}
+//int year = Method3();
 // Console.WriteLine(year);
 
 //Вид 4
@@ -46,26 +46,26 @@ int year = Method3();
 //string res = Method4(10, "asdf");
 //Console.WriteLine(res);
 
-string Method4(int count, string c)
-{
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
-    {
-        result = result + c;
-    }
-    return result;
-}
-string res = Method4(10, "asdf");
+//string Method4(int count, string c)
+//{
+//    string result = String.Empty;
+//    for (int i = 0; i < count; i++)
+//    {
+//        result = result + c;
+//    }
+//    return result;
+//}
+//string res = Method4(10, "asdf");
 //Console.WriteLine(res);
 
-for(int i = 2; i <= 10; i++)
-{
-    for (int j = 2; j <= 10; j++)
-    {
-//        Console.WriteLine($"{i} x {j} = {i * j}");
-    }
-//    Console.WriteLine();
-}
+//for (int i = 2; i <= 10; i++)
+//{
+//    for (int j = 2; j <= 10; j++)
+//    {
+        //        Console.WriteLine($"{i} x {j} = {i * j}");
+//    }
+    //    Console.WriteLine();
+//}
 
 //===== Работа с текстом
 // Дане текст. В тексте нужно все пробелы заменить черточками,
@@ -74,32 +74,62 @@ for(int i = 2; i <= 10; i++)
 
 // Ясна ли задача?
 
-string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
-            + "ежели бы вас послали вместо нашего милого Винценгероде,"
-            + "вы бы взяли приступом согласие прусского короля. "
-            + " Вы так красоречивы. Вы дадите мне чаю?";
+//string text = "- Я думаю, - сказал князь, улыбаясь, - что, "
+//            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+//            + "вы бы взяли приступом согласие прусского короля. "
+//           + " Вы так красоречивы. Вы дадите мне чаю?";
 
 // string s = "asd"
 //             012
 // s[2] //  d
 
-Console.OutputEncoding = System.Text.Encoding.UTF8;
-string Replace(string text, char oldValue, char newValue)
+//Console.OutputEncoding = System.Text.Encoding.UTF8;
+//string Replace(string text, char oldValue, char newValue)
+//{
+//    string result = String.Empty;
+//    int length = text.Length;
+//    for (int i = 0; i < length; i++)
+//    {
+//        if (text[i] == oldValue) result = result + $"{newValue}";
+//        else result = result + $"{text[i]}";
+//    }
+//    return result;
+//}
+//string newText = Replace(text, ' ', '|');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'к', 'К');
+//Console.WriteLine(newText);
+//Console.WriteLine();
+//newText = Replace(newText, 'С', 'с');
+//Console.WriteLine(newText);
+
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
 {
-    string result = String.Empty;
-    int length = text.Length;
-    for (int i = 0; i < length; i++)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        if(text[i] == oldValue) result = result + $"{newValue}";
-        else result = result + $"{text[i]}";
+        Console.Write($"{array [i]} ");
     }
-    return result;
+    Console.WriteLine();
 }
-string newText = Replace(text, ' ', '|');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'к', 'К');
-Console.WriteLine(newText);
-Console.WriteLine();
-newText = Replace(newText, 'С', 'с');
-Console.WriteLine(newText);
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
